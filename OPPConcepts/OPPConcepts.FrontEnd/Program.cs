f1761da3c98dbed42edc9055033e3f40dbbedbdf
Sebastian López <sebasgal608@gmail.com>
2026-02-12 21:16:35 -0500

    Exercice Date POO

---
+++ OPPConcepts/OPPConcepts.FrontEnd/Program.cs
@@ -0,0 +1,16 @@
+﻿using OPPConcepts.Backed;
+
+try
+{
+    var day1 = new Date();
+    var day2 = new Date(2026, 29, 2);
+    var day3 = new Date(2012, 12, 10);
+
+    Console.WriteLine(day1);
+    Console.WriteLine(day2);
+    Console.WriteLine(day3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}

b46646b83853fb679d8fa83a30b5d30c58618fb5
Sebastian López <sebasgal608@gmail.com>
2026-02-20 13:40:52 -0500

    First class diagram of the file Date.cs and file Date.cs reinforced

--- OPPConcepts/OPPConcepts.Backed/Date.cs
+++ OPPConcepts/OPPConcepts.Backed/Date.cs
@@ -1,86 +1,4 @@
 ﻿namespace OPPConcepts.Backed;
-
-public class Date
-{
-    private int _day;
-    private int _month;
-    private int _year;
-    public Date()
-
-    {
-        _year = 1900;
-        _month = 1;
-        _day = 1;
-
-    }
-    public Date(int year, int month, int day)
-
-    {
-        _year = ValidateYear(year);
-        _month = ValidateMonth(month);
-        _day = ValidateDay(day);
-    }
-    public int Day
-    {
-        get => _day;
-        set => _day = ValidateDay(value);
-    }
-    public int Month
-    {
-        get => _month;
-        set => _month = ValidateMonth(value);
-    }
-    public int Year
-    {
-        get => _year;
-        set => _year = ValidateYear(value);
-    }
-    public override string ToString()
-    {
-        return $"{_year:0000}/{_month:00}/{_day:00}";
-    }
-    private int ValidateYear(int year)
-    {
-        if (year < 0)
-        {
-            throw new ArgumentException($"The year: {year}, not is valid.");
-        }
-        return year;
-    }
-    private int ValidateMonth(int month)
-    {
-        if (month < 1 || month > 12)
-        {
-            throw new ArgumentException($"The month: {month}, not is valid.");
-        }
-        return month;
-    }
-    private int ValidateDay(int day)
-    {
-        if (day < 1)
-        {
-            throw new ArgumentException($"The day: {day}, not is valid.");
-        }
-        if (day == 29 && _month == 2 && IsLeapYear(_year))
-        {
-            return day;
-        }
-        if ((day <= 28 && _month == 2) || (day <= 30 && (_month == 4 || _month == 6 || _month == 9 || _month == 11))
-||
- (day <= 31 && (_month == 1 || _month == 3 || _month == 5 || _month == 7 ||
-_month == 8 || _month == 10 || _month == 12)))
-        {
-            return day;
-        }
-        throw new ArgumentException($"The day: {day}, not is valid.");
-    }
-    private bool IsLeapYear(int year)
-    {
-        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
-    }
-}
-
-/*
 public class Date
 {
     private int _year;
@@ -101,13 +19,13 @@ public class Date
         Day = day;
     }
 
-    public int Year 
+    public int Year
     {
-        get => _year; 
-        set => _year = ValidateYear(value);    
+        get => _year;
+        set => _year = ValidateYear(value);
     }
-  
-    public int Month 
+
+    public int Month
     {
         get => _month;
         set => _month = ValidateMonth(value);
@@ -126,7 +44,7 @@ public class Date
 
     private int ValidateYear(int year)
     {
-        if(year < 0)
+        if (year < 0)
         {
             throw new ArgumentOutOfRangeException(nameof(year), "Year must be greathest than 0.");
         }
@@ -135,7 +53,7 @@ public class Date
 
     private int ValidateMonth(int month)
     {
-        if(month < 1 || month > 12)
+        if (month < 1 || month > 12)
         {
             throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12.");
         }
@@ -166,4 +84,4 @@ public class Date
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
-*/
+
--- OPPConcepts/OPPConcepts.FrontEnd/Program.cs
+++ OPPConcepts/OPPConcepts.FrontEnd/Program.cs
@@ -1,37 +1,16 @@
 ﻿using OPPConcepts.Backed;
+
 try
 {
-    var employee1 = new SalaryEmployee(1, "Patricia", "Morales", new Date(2020, 4, 15),
-new Date(1981, 1, 12), true, 2500000);
-    var employee2 = new HourlyEmployee(2, "Ricardo", "Sanchez", new Date(2021, 12, 5),
-   new Date(1971, 8, 11), true, 23000, 156);
-    var employee3 = new CommisionEmployee(3, "Monica", "Correa", new Date(2022, 5, 5),
-   new Date(1985, 2, 11), true, 0.03f, 290000000);
-    var employee4 = new BaseCommisionEmployee(3, "Sandra", "Jaramillo", new Date(2022,
-   5, 5), new Date(1985, 2, 11), true, 0.015f, 120000000, 1100000);
-    var employees = new List<Employee>() { employee1, employee2, employee3, employee4 };
-    decimal total = 0;
-    Console.WriteLine("PAY ROL");
-    foreach (var employee in employees)
-    {
-        Console.WriteLine(employee);
-        Console.WriteLine();
-        total += employee.GetValueToPay();
-    }
-    var invoice1 = new Invoice(1, "Chair", new Date(2025, 2, 5), 4, 350000);
-    var invoice2 = new Invoice(2, "Computer", new Date(2025, 2, 5), 2, 2850000);
-    var invoice3 = new Invoice(3, "Paper", new Date(2025, 2, 5), 10, 56000);
-    var invoices = new List<Invoice>() { invoice1, invoice2, invoice3 };
-    Console.WriteLine("INVOICES");
-    foreach (var invoice in invoices)
-    {
-        Console.WriteLine(invoice);
-        Console.WriteLine();
-        total += invoice.GetValueToPay();
-    }
-    Console.WriteLine($"TOTAL..................: {total,15:C2}");
+    var date1 = new Date();
+    var date2 = new Date(2026, 2, 28);
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex);
+    Console.WriteLine(ex.Message);
 }

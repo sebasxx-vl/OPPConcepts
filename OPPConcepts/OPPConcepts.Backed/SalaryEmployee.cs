b31e51fc88890fbe2871ea31817d90240eedc477
Sebastian López <sebasgal608@gmail.com>
2026-02-19 20:07:01 -0500

    Employee

---
+++ OPPConcepts/OPPConcepts.Backed/SalaryEmployee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPPConcepts.Backed;
+public class SalaryEmployee : Employee
+{
+
+}
